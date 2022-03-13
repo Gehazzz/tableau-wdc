03c35fcceee160e05317afc26385692ee0cbc4f8
gennadyzaytsev <gennadyza@papayaglobal.com>
2022-03-13 20:36:22 +0200

    init

---
+++ src/main/java/com/proteantec/tableauwdc/TableauWdcApplication.java
@@ -0,0 +1,13 @@
+package com.proteantec.tableauwdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableauWdcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableauWdcApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/proteantec/tableauwdc/controller/TableauWebConnectorController.java
@@ -0,0 +1,7 @@
+package com.proteantec.tableauwdc.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TableauWebConnectorController {
+}
